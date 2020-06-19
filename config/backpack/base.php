76e6251a451f71b792287fadf2bd85f40ed9dfcd
Heles Junior <helesjunior@gmail.com>
2020-06-19 13:46:10 -0300

    Pequenas alterações template

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -71,13 +71,13 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => 'Conta',
+    'project_logo'   => 'Sistema<b>Conta</b>',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
 
     // Horizontal navbar classes. Helps make the admin panel look similar to your project's design.
-    'header_class' => 'app-header navbar navbar-color bg-primary border-0',
+    'header_class' => 'app-header navbar navbar-color bg-orange border-0',
         // For background colors use: bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
         // For links to be visible on different background colors use: "navbar-dark", "navbar-light", "navbar-color"
 
@@ -100,18 +100,18 @@ return [
     // ------
 
     // Footer element classes.
-    'footer_class' => 'app-footer bg-secondary',
+    'footer_class' => 'app-footer bg-dark',
         // hide it with d-none
         // change background color with bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Conta',
+    'developer_name' => 'Sistema Conta',
 
     // Developer website. Link in footer. Type false if you want to hide it.
     'developer_link' => 'https://github.com/helesjunior/conta',
 
     // Show powered by Laravel Backpack in the footer? true/false
-    'show_powered_by' => true,
+    'show_powered_by' => false,
 
     // -------
     // SCRIPTS
